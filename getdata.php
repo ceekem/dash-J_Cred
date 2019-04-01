9129f219040e65a6699bcadda12ec64d662d0410
ceekem <37778925+ceekem@users.noreply.github.com>
2019-04-01 16:30:46 +0200

    revert vuyo
    i had to revert vuyos code

--- getdata.php
+++ getdata.php
@@ -1,14 +1,11 @@
 <?php
-
 include 'php/db.php';
-$sel = mysqli_query($conn, "SELECT users.uid, users.date_added, users.fullname, users.phone, users.email, users.address, users.city, users.code, users.password, users.type, users.org, users.avatar, users.cover, COUNT(documents.document) AS docNo
-                            FROM users, documents WHERE users.email = documents.email AND users.type NOT LIKE('%Admin')");
-//$sel = mysqli_query($conn, "select * from users WHERE type not like '%Admin'");
-// $sel2 = mysqli_query($conn, "SELECT COUNT(documents.document) FROM users,documents WHERE users.email = documents.email AND  users.email = 'koni@gmail.com'"; 
 
+$sel = mysqli_query($conn, "select * from users where type not like '%Admin'");
 $data = array();
 
-    while($row = mysqli_fetch_array($sel)){
+
+while($row = mysqli_fetch_array($sel)){
    
     $data[] = array("id"=>$row['uid'],
                     "fullname" =>$row['fullname'], 
@@ -16,11 +13,8 @@ $data = array();
                     "type" =>$row['type'],
                     "org" => $row['org'],
                     "email" =>$row['email'],
-                    "status" =>$row['mStatus'],
-                    "docNo" =>$row['docNo']);
+                    "status" =>$row['mStatus']);
 }
 echo json_encode($data);
-
-
 
 ?>
